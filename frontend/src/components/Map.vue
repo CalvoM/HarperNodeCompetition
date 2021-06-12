f93cbc90f8c1858e172f443aab76f5084d5f006a
CalvoM <mwadimemakokha@gmail.com>
2021-06-12 21:30:26 +0300

    Basic map

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="map">
+        <MglMap :accessToken="accessToken" :mapStyle="mapstyle" @load="onMapLoaded"/>
+    </div>
+</template>
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import Mapbox from 'mapbox-gl';
+import {MglMap} from 'vue-mapbox';
+
+@Options({
+    components:{
+        MglMap,
+    }
+})
+export default class Mapping extends Vue{
+    mapbox! : any 
+    data(){
+        return {
+            accessToken: 'pk.eyJ1IjoiZDFyM2N0MHIiLCJhIjoiY2twdHZnZzhpMHR2aDJvdDhiZmJuemxnaSJ9.12nIcmk0giCqU8XgK13W4Q',
+            mapstyle: 'mapbox://styles/mapbox/streets-v11'
+        }
+    }
+    created(){
+        this.mapbox = null;
+    }
+    onMapLoaded(event: any):any{
+        console.log(event)
+        this.mapbox = event.map
+    }
+}
+</script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Mapping />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Mapping from '@/components/Map.vue'
+
+@Options({
+  components: {
+    HelloWorld,
+    Mapping
+  },
+})
+export default class Home extends Vue {}
+</script>
